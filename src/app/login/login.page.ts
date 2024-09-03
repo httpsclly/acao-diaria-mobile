98c6543422bc602f7daf2c95804040b513f9df17
httpsclly <marcellybrito44@gmail.com>
2024-09-03 13:37:01 -0300

    Login Atualização - 03/09 - Às 13:24

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -19,4 +19,20 @@ export class LoginPage {
     // Simula um login bem-sucedido e redireciona para a página principal das tabs
     this.navCtrl.navigateRoot('/tabs');
   }
+
+  usernameType: string = 'text';
+  passwordType: string = 'password';
+  usernameIcon: string = 'eye-off';
+  passwordIcon: string = 'eye-off';
+
+  toggleUsernameVisibility() {
+    this.usernameType = this.usernameType === 'text' ? 'password' : 'text';
+    this.usernameIcon = this.usernameType === 'text' ? 'eye-off' : 'eye';
+  }
+
+  togglePasswordVisibility() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordType === 'text' ? 'eye-off' : 'eye';
+  }
 }
+
